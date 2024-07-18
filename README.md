5965cb827efff395ac09d0b2670275b5a6938c1b
bmgwaro <brian.gwaro@student.moringaschool.com>
2024-07-18 19:04:27 +0300

    music app

---
+++ README.md
@@ -0,0 +1,58 @@
+# Music Application
+
+## Description
+
+Originally, this application was meant to play the music videos of every available song. However, due to copyright issues I was unable to embed the videos into my code and opted to improvise. 
+As a user, you will be able to view a list of songs, see detailed information about each song, and add new songs to the list. You also have the option to to download songs for offline listening.
+
+## Features
+   - Fetches and displays a list of songs from the server.
+   - Highlights songs on mouseover.
+   - Loads song details on click.
+   - Displays detailed information about a selected song, including name, artist, genre, and a link to the song.
+   - Allows users to download a song and displays a confirmation message.
+   - Provides a form to add new songs to the list.
+   - Submits the new song to the server and updates the song list.
+
+## Prerequisites
+- Operating system
+- Code editor
+- JSON server
+- Browser
+- 2GB free disk space
+
+## Getting Started
+1. Click on this link in order to access the github repository containing this project;
+[https://github.com/bmgwaro/Simple-Music-App-Phase-1-project-]
+
+2. Click on fork and create a fork of the repository. 
+
+3. Open your terminal and navigate into the directory where you would like to save the work using the `cd` command.
+
+4. Feel free to copy and paste the following command in order to clone the repository into your local storage; 
+`git clone git@github.com:bmgwaro/Simple-Music-App-Phase-1-project-.git`
+
+5. Navigate into the newly cloned folder and type in the `code .` command in order to open it on your text editor.
+
+## How to Use the Application
+
+1. Load the Application:
+When the page loads, the JavaScript fetches the list of available songs from the server and displays them.
+
+2. Interact with the Song List:
+Each song in the list is clickable. Clicking on a song fetches its details and displays them in the main section.
+
+3. Select a Song:
+Click on any song name in the list to view its details.
+
+4. Song Details Display:
+The details include the song's name, artist, genre, and a link to the song image. These are all displayed in the webpage.
+
+5. Download a Song:
+After viewing a song's details, click the "Download" button.
+A message "This song has been downloaded successfully" will be displayed under the song details.
+
+6. Adding a New Song
+- Enter the song name, artist, genre, and link in the form located at the bottom of the page.
+- Click the "Add Song" button to add the new song to the playlist. This sends a POST request to the server with the new song's data.
+- The new song will be added to the song list, and you can click on it to view its details just like the existing songs.
---
+++ app.js
@@ -0,0 +1,113 @@
+const url = "http://localhost:3000/songs";
+
+document.addEventListener("DOMContentLoaded", () => {
+  fetch(`${url}/2`)
+    .then((res) => res.json())
+    .then((data) => displaySong(data));
+});
+
+fetch(`${url}`)
+  .then((res) => res.json())
+  .then((data) => displaySongList(data))
+  .then((data)=>addNewSong(data))
+
+function displaySongList(songList) {
+  const songs = document.getElementById("songNames");
+
+  songList.forEach((song) => {
+    let eachSong = document.createElement("li");
+    eachSong.textContent = song.name;
+    songs.classList.add("slide-in")
+    songs.appendChild(eachSong);
+    eachSong.addEventListener("click", (event) => {
+      event.preventDefault();
+      fetch(`${url}/${song.id}`)
+        .then((res) => res.json())
+        .then((data) => displaySong(data));
+    });
+
+    eachSong.addEventListener("mouseover", (event) => {
+      event.target.style.backgroundColor = "#f0f0f0";
+    });
+
+    eachSong.addEventListener("mouseout", (event) => {
+      event.target.style.backgroundColor = "";
+    });
+  });
+}
+
+function displaySong(song) {
+  const name = document.getElementById("name");
+  const vid = document.getElementById("link");
+  const artist = document.getElementById("artist");
+  const genre = document.getElementById("genre");
+
+  name.textContent = song.name;
+  vid.src = song.link;
+  artist.textContent = song.artist;
+  genre.textContent = song.genre;
+
+  const downloaded = document.getElementById("downloaded");
+  downloaded.innerHTML = "";
+
+  downloadSong();
+}
+
+function downloadSong() {
+  const download = document.getElementById("download");
+
+  download.addEventListener("click", (event) => {
+    event.preventDefault();
+    const downloaded = document.getElementById("downloaded");
+    downloaded.textContent = "This song has been downloaded succesfully.";
+  });
+}
+
+function addNewSong() {
+  const newSong = document.getElementById("songForm");
+
+  newSong.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const formData = {
+      name: document.getElementById("nameInput").value,
+      artist: document.getElementById("artistInput").value,
+      genre: document.getElementById("genreInput").value,
+      link: document.getElementById("linkInput").value
+    };
+
+    fetch(`${url}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((res) => res.json())
+      .then((newSong) => {
+        const songsList = document.getElementById("songNames");
+        const newSongItem = document.createElement("li");
+        newSongItem.textContent = newSong.name;
+        songsList.appendChild(newSongItem);
+
+        newSongItem.addEventListener("click", (event) => {
+          event.preventDefault();
+          fetch(`${url}/${newSong.id}`)
+            .then((res) => res.json())
+            .then((data) => displaySong(data));
+        });
+
+        document.getElementById("songForm").reset();
+
+        newSongItem.addEventListener("mouseover", (event) => {
+          event.target.style.backgroundColor = "#f0f0f0";
+        });
+    
+        newSongItem.addEventListener("mouseout", (event) => {
+          event.target.style.backgroundColor = "";
+        });
+      })
+      .catch((error) => console.log(error));
+  });
+}
